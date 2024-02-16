fc4d987f65a68e0bf372672034227d901365c86c
sablals <158267211+sablals@users.noreply.github.com>
2024-02-16 10:42:23 +0530

    Initial commit

---
+++ exceptionhandiling/src/main/java/samplecollections/Setttt.java
@@ -0,0 +1,22 @@
+package samplecollections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Setttt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	List<String> data = new ArrayList<String>();   
+	    
+        data.add("JavaTpoint");   
+        data.add("Set");   
+        data.add("Example");   
+        data.add("Set");   
+    
+        System.out.println(data);
+    
+        System.out.println(data);   
+	}
+
+}
